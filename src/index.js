72dc2fc08a38a54c205752bfa37ce6a1a57def8a
mcnwr76 <anwaraan998@gmail.com>
2025-02-18 11:19:45 +0700

    tetst localhost:4000

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Counter } from "k6/metrics";
 export const options = {
   thresholds: {
     server_bun_get_success: ['count>190'],
-    server_node_get_success: ['count>190'],
+    // server_node_get_success: ['count>190'],
   },
   scenarios: {
     bunServer: {
@@ -13,12 +13,12 @@ export const options = {
       vus: 10,
       duration: "10s"
     },
-    nodeServer: {
-      exec: "nodeServer", // Must match a function name
-      executor: "constant-vus",
-      vus: 10,
-      duration: "10s"
-    }
+    // nodeServer: {
+    //   exec: "nodeServer", // Must match a function name
+    //   executor: "constant-vus",
+    //   vus: 10,
+    //   duration: "10s"
+    // }
   }
 };
 
@@ -26,15 +26,15 @@ const bunServerGetSuccess = new Counter("server_bun_get_success");
 const nodeServerGetSuccess = new Counter("server_node_get_success");
 
 export function bunServer() {
-  const res = http.get('http://localhost:3000');
+  const res = http.get('http://localhost:4000');
   if (res.status === 200) {
     bunServerGetSuccess.add(1);
   }
 }
 
-export function nodeServer() {
-  const res = http.get('http://localhost:3001');
-  if (res.status === 200) {
-    nodeServerGetSuccess.add(1);
-  }
-}
+// export function nodeServer() {
+//   const res = http.get('http://localhost:3001');
+//   if (res.status === 200) {
+//     nodeServerGetSuccess.add(1);
+//   }
+// }
