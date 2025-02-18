9d68e51d1bc4e5206a21f8bfc8b984206c134fb9
mcnwr76 <anwaraan998@gmail.com>
2025-02-18 15:32:36 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,37 +2,47 @@ import { sleep } from 'k6';
 import http from 'k6/http';
 import { Counter } from "k6/metrics";
 
-export const options = {
+const opt1 = {
   thresholds: {
     server_bun_get_success: ['count>190'],
-    // server_node_get_success: ['count>190'],
+    server_node_get_success: ['count>190'],
   },
   scenarios: {
     bunServer: {
-      exec: "bunServer", // Must match a function name
+      exec: "bunServer", //
       executor: "constant-vus",
       vus: 10,
       duration: "60s"
     },
-    // nodeServer: {
-    //   exec: "nodeServer", // Must match a function name
-    //   executor: "constant-vus",
-    //   vus: 10,
-    //   duration: "10s"
-    // }
+    nodeServer: {
+      exec: "nodeServer",
+      executor: "constant-vus",
+      vus: 10,
+      duration: "10s"
+    }
   }
 };
 
-const bunServerGetSuccess = new Counter("server_bun_get_success");
+const opt2 = {
+  stages: [
+    { duration: '30s', target: 50 },
+    { duration: '1m', target: 100 },
+    { duration: '30s', target: 0 }
+  ]
+}
+
+export const options = opt2
+
+// const bunServerGetSuccess = new Counter("server_bun_get_success");
 // const nodeServerGetSuccess = new Counter("server_node_get_success");
 
-export function bunServer() {
-  const res = http.get('http://localhost:4000');
-  if (res.status === 200) {
-    bunServerGetSuccess.add(1);
-  }
-  // sleep(1)
-}
+// export function bunServer() {
+//   const res = http.get('http://202.157.185.246:4000');
+//   if (res.status === 200) {
+//     bunServerGetSuccess.add(1);
+//   }
+//   sleep(1)
+// }
 
 // export function nodeServer() {
 //   const res = http.get('http://localhost:3001');
@@ -40,3 +50,10 @@ export function bunServer() {
 //     nodeServerGetSuccess.add(1);
 //   }
 // }
+
+export function test() {
+  const res = http.get('http://localhost:4000');
+  if (res.status === 200) {
+    nodeServerGetSuccess.add(1);
+  }
+}
